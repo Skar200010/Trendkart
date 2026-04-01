d86140da52e9debb900f44251087fd63add7fd0a
Skar200010 <khedekarsohan10@gmail.com>
2026-04-01 19:50:08 +0530

    install script of cuelink

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,6 +91,18 @@ export default function RootLayout({
           `}
         </Script>
         <Script src="https://www.noupe.com/embed/019d1a622fdc7926b1238991668cc362c54d.js" strategy="afterInteractive" />
+        <Script id="cuelinks-widget" strategy="afterInteractive">
+          {`
+            var cId = '271326';
+            (function(d, t) {
+              var s = document.createElement('script');
+              s.type = 'text/javascript';
+              s.async = true;
+              s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/') + 'cuelinksv2.js';
+              document.getElementsByTagName('body')[0].appendChild(s);
+            }());
+          `}
+        </Script>
       </body>
     </html>
   );
